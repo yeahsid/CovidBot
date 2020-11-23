a10974bd1f71d325266c20c8775db0e85a5552b0
Siddharth Iyer <siddharth1912@outlook.com>
2020-11-23 17:53:57 +0100

    v1

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from ariadne import ObjectType, QueryType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
+import sys
+sys.path.append('modules/')
+import functions 
 
 # Define types using Schema Definition Language (https://graphql.org/learn/schema/)
 # Wrapping string in gql function provides validation and better error traceback
@@ -31,3 +34,5 @@ schema = make_executable_schema(type_defs, query)
 
 # Create an ASGI app using the schema, running in debug mode
 app = GraphQL(schema, debug=True)
+
+data = functions.api_call()
